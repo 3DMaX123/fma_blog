288c7212e4e33b37294c10a24aef0d33500c51e7
3DMaX123 <3dmax214@gmail.com>
2022-05-15 20:22:16 +0300

    IDK some changes

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Footer = () => {
-  return (
-    <div>Footer</div>
+import './NavBar.css';
+import images from '../../constants/images.js'; 
+
+const NavBar = () => {
+
+  const [ grad, setGrad ] = useState(false);
+
+  return(
+    <div className='app__navbar'>
+      <div className='navbar-socialmedia'>
+
+      </div>
+      <div className='Logo'>
+        <object className='mainLogo' data={images.mainLogo}>Feed Motion Blog logo</object>
+      </div>
+      <div className='menuButton'>
+        <img className='burgerMenu' src={images.menubar} alt='Menu bar(burger)' onMouseEnter={() => setGrad(true)} onMouseLeave={() => setGrad(false)}/>
+      </div>
+    </div>
   )
 }
 
-export default Footer
+export default NavBar
--- src/constants/images.js
+++ src/constants/images.js
@@ -1,5 +1,15 @@
-import bench from '';
+import mainLogo from '../assets/mainLogo.svg';
+import facebook from '../assets/facebook.svg';
+import twitter from '../assets/twitter.svg';
+import instagram from '../assets/instagram.svg';
+import menubar from '../assets/menuBar.svg';
+import menubargrad from '../assets/menuBarGrad.svg';
 
 export default{
-    bench
+    menubargrad,
+    mainLogo,
+    facebook,
+    twitter,
+    instagram,
+    menubar
 };
