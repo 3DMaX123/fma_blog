2f1e85c9ca595ed29f835fdc4c9c69d84814a2f5
3DMaX123 <3dmax214@gmail.com>
2022-05-21 13:29:24 +0300

    add emojy

--- src/constants/images.js
+++ src/constants/images.js
@@ -5,8 +5,42 @@ import instagram from '../assets/instagram.svg';
 import menubar from '../assets/menuBar.svg';
 import menubargrad from '../assets/menuBarGrad.svg';
 import example from '../assets/example.jpg';
+import emojy_angel from '../assets/emojy-angel.svg';
+import emojy_awkward from '../assets/emojy-awkward.svg';
+import emojy_cool from '../assets/emojy-cool.svg';
+import emojy_demon from '../assets/emojy-demon.svg';
+import emojy_dntcare from '../assets/emojy-dntcare.svg';
+import emojy_dumbsmile from '../assets/emojy-dumbsmile.svg';
+import emojy_happy from '../assets/emojy-happy.svg';
+import emojy_happyred from '../assets/emojy-happyred.svg';
+import emojy_inlove from '../assets/emojy-inlove.svg';
+import emojy_inshock from '../assets/emojy-inshock.svg';
+import emojy_intotalshock from '../assets/emojy-intotalshock.svg';
+import emojy_laugh from '../assets/emojy-laugh.svg';
+import emojy_loveya from '../assets/emojy-loveya.svg';
+import emojy_pleasure from '../assets/emojy-pleasure.svg';
+import emojy_shock from '../assets/emojy-shock.svg';
+import emojy_tongue from '../assets/emojy-tongue.svg';
+import emojy_winks from '../assets/emojy-winks.svg';
 
 export default{
+    emojy_angel,
+    emojy_awkward,
+    emojy_cool,
+    emojy_demon,
+    emojy_dntcare,
+    emojy_dumbsmile,
+    emojy_happy,
+    emojy_happyred,
+    emojy_inlove,
+    emojy_inshock,
+    emojy_intotalshock,
+    emojy_laugh,
+    emojy_loveya,
+    emojy_pleasure,
+    emojy_shock,
+    emojy_tongue,
+    emojy_winks,
     example,
     menubargrad,
     mainLogo,
